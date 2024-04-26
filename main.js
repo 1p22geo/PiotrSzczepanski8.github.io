f8b9f4c278672eba41bf871652cc024d11b271bd
1p22geo <1p22geodecki@gmail.com>
2024-04-26 13:12:29 +0200

    feat: added DB connection

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 const express = require('express')
+const mongodb = require("mongodb")
 const app = express()
 const port = 3000
 
+const MONGO_URI = "mongodb://minisforum:27017"
+
 app.use(express.static('docs'))
 
-app.get('/helo', (req, res) => {
-  res.send('Hello World!')
+app.get('/helo', async (req, res) => {
+  const client = new mongodb.MongoClient(MONGO_URI)
+  await client.connect()
+  const db = client.db("xmail")
+  const users = db.collection("users")
+  const data = await users.find({}).toArray()
+  res.write(JSON.stringify(data))
 })
 
 app.listen(port, () => {
